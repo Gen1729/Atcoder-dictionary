ea912782a69d0899039e30944f3ad77278287905
irinoirino <genkishoot@icloud.com>
2025-07-23 21:02:11 +0900

    検索システム実装

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, watch } from 'vue'
 
 type Ind = {
   name: string
@@ -8,12 +8,21 @@ type Ind = {
 
 const pong = ref<Ind[]>()
 
-onMounted(async () => {
-  const res = await fetch('/api/dict?words=A,B,for')
+const checkedWords = ref<string[]>([])
+
+async function searchWords() {
+  let s: string = ''
+
+  for (let i: number = 0; i < checkedWords.value?.length; i++) {
+    s += checkedWords.value[i]
+    if (i == checkedWords.value?.length - 1) break
+    s += ','
+  }
+  const res = await fetch('/api/dict?words=' + s)
   if (res.ok) {
     pong.value = await res.json()
   }
-})
+}
 </script>
 
 <template>
@@ -22,6 +31,15 @@ onMounted(async () => {
       <a :href="item.url" target="_blank">{{ item.name }}</a>
     </div>
   </div>
+  <button @click="searchWords">検索</button>
+  <h2>Multi Checkbox</h2>
+  <input type="checkbox" id="abc" value="ABC" v-model="checkedWords" />
+  <label for="abc">ABC</label>
+  <input type="checkbox" id="a" value="A" v-model="checkedWords" />
+  <label for="a">A</label>
+  <input type="checkbox" id="b" value="B" v-model="checkedWords" />
+  <label for="b">B</label>
+  {{ checkedWords }}
 </template>
 
 <style></style>
